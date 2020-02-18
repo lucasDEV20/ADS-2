189c4886756fcbae947218a7907976656d29dd01
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-02-18 18:35:16 -0300

    Add files via upload

---
+++ lista de POO/piramede/src/classes/piramde/piramede.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.piramde;
+
+/**
+ *
+ * @author lab03
+ */
+//atributos
+public class piramede {
+
+    private String nome;
+    private float peso;
+    private int altura;
+    private String sexo;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the peso
+     */
+    public float getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the altura
+     */
+    public int getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public float calculariMC() {
+        return (peso / altura * altura);
+        
+    }
+
+}
